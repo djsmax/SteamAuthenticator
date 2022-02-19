915684e3ab4d0d6df5c93f699ce6969979ebcc9e
Max <lesa_an@inbox.ru>
2022-02-19 11:13:19 +0100

    Initial Commit

---
+++ SteamAuthenticator/SteamAuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamAuthenticatorApp.swift
+//  SteamAuthenticator
+//
+//  Created by Max Denwer on 19.02.22.
+//
+
+import SwiftUI
+
+@main
+struct SteamAuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
